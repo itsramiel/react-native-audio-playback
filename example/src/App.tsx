140d25f945f3092d77d07b715efb35acc5eebaf4
Rami Elwan <ramielwan48@gmail.com>
2024-11-21 20:06:25 +0100

    feat: finalize demo app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,101 +1,194 @@
-import { Fragment, useState } from 'react';
-import { StyleSheet, View, Button } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  type PressableProps,
+  Pressable,
+  Text,
+  type ViewStyle,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
 import { Player, AudioManager } from 'react-native-audio-playback';
 
 const audioManager = new AudioManager(44100, 2);
 audioManager.openAudioStream();
 
 export default function App() {
-  const [player1, setPlayer1] = useState<Player | null>(null);
-  const [player2, setPlayer2] = useState<Player | null>(null);
+  const [players, setPlayers] = useState<
+    Array<{ player: Player; title: string }>
+  >([]);
 
-  const players = [player1, player2].filter(Boolean) as Array<Player>;
+  useEffect(() => {
+    (async () => {
+      const players = Array<{ player: Player; title: string }>();
+      const player1 = await audioManager.loadSound(
+        require('./assets/bamboo.mp3')
+      );
+      if (player1) {
+        players.push({ player: player1, title: 'Bamboo' });
+      }
+      const player2 = await audioManager.loadSound(
+        require('./assets/swords.mp3')
+      );
+      if (player2) {
+        players.push({ player: player2, title: 'Swords' });
+      }
+      const player3 = await audioManager.loadSound(
+        require('./assets/coins.mp3')
+      );
+      if (player3) {
+        players.push({ player: player3, title: 'Coins' });
+      }
+
+      const player4 = await audioManager.loadSound(require('./assets/axe.mp3'));
+
+      if (player4) {
+        players.push({ player: player4, title: 'Axe' });
+      }
+
+      setPlayers(players);
+    })();
+  }, []);
 
   return (
-    <View style={styles.container}>
-      <Button
-        title="Play sounds"
-        onPress={() => {
-          audioManager.playSounds(players.map((player) => [player, true]));
-        }}
-      />
-      <Button
-        title="Loop sounds"
-        onPress={() => {
-          audioManager.loopSounds(players.map((player) => [player, true]));
-        }}
-      />
-      <Button
-        title="Seek sounds"
-        onPress={() => {
-          audioManager.seekSoundsTo([
-            [player1!, 5000],
-            [player2!, 5000],
-          ]);
-        }}
-      />
-      {[
-        [player1, setPlayer1, require('./assets/CLAP-output1.mp3')] as const,
-        [player2, setPlayer2, require('./assets/FUNKY_HOUSE.wav')] as const,
-      ].map(([player, setPlayer, asset], index) => (
-        <Fragment key={index}>
-          {player ? (
-            <>
-              <Button
-                title={`play sound ${index}`}
-                onPress={async () => {
-                  player.playSound();
-                }}
-              />
-              <Button
-                title={`pause sound ${index}`}
-                onPress={() => {
-                  player.pauseSound();
-                }}
-              />
-              <Button
-                title={'Seek to 5 seconds'}
-                onPress={() => {
-                  player.seekTo(5000);
-                }}
-              />
-              <Button
-                title={`loop sound ${index}`}
-                onPress={() => {
-                  player.loopSound(true);
-                }}
-              />
-              <Button
-                title={`unload sound ${index}`}
-                onPress={() => {
-                  player.unloadSound();
-                  setPlayer(null);
-                }}
-              />
-            </>
-          ) : (
-            <Button
-              title={`load sound ${index}`}
-              onPress={async () => {
-                audioManager.loadSound(asset).then(setPlayer);
-              }}
-            />
-          )}
-        </Fragment>
-      ))}
+    <SafeAreaView style={styles.screen}>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        {players.map((player) => (
+          <PlayerControls
+            key={player.player.id}
+            title={player.title}
+            player={player.player}
+          />
+        ))}
+        <PlayersControls
+          players={players
+            .filter((p) => p.title !== 'Axe')
+            .map((player) => player.player)}
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+interface PlayersControlsProps {
+  players: Array<Player>;
+}
+
+function PlayersControls({ players }: PlayersControlsProps) {
+  return (
+    <View style={styles.controlsContainer}>
+      <Text style={styles.controlsTitle}>All players</Text>
+      <View style={styles.controlsInnerContainer}>
+        <Button
+          title="Play all"
+          onPress={() =>
+            audioManager.playSounds(players.map((player) => [player, true]))
+          }
+        />
+        <Button
+          title="Pause all"
+          onPress={() =>
+            audioManager.playSounds(
+              players.map((player) => [player, false] as const)
+            )
+          }
+        />
+        <Button
+          title="Loop all"
+          onPress={() =>
+            audioManager.loopSounds(
+              players.map((player) => [player, true] as const)
+            )
+          }
+        />
+        <Button
+          title="Unloop all"
+          onPress={() =>
+            audioManager.loopSounds(
+              players.map((player) => [player, false] as const)
+            )
+          }
+        />
+      </View>
     </View>
   );
 }
 
+interface PlayerControlsProps {
+  title: string;
+  player: Player;
+}
+
+function PlayerControls({ title, player }: PlayerControlsProps) {
+  return (
+    <View style={styles.controlsContainer}>
+      <Text style={styles.controlsTitle}>{title}</Text>
+      <View style={styles.controlsInnerContainer}>
+        <Button title="Play" onPress={() => player.playSound()} />
+        <Button title="Pause" onPress={() => player.pauseSound()} />
+        <Button title="Seek To 5 secs" onPress={() => player.seekTo(5000)} />
+        <Button title="loop" onPress={() => player.loopSound(true)} />
+        <Button title="unloop" onPress={() => player.loopSound(false)} />
+      </View>
+    </View>
+  );
+}
+
+interface ButtonProps extends PressableProps {
+  title: string;
+  style?: ViewStyle;
+}
+
+function Button({ title, ...props }: ButtonProps) {
+  return (
+    <Pressable
+      {...props}
+      style={({ pressed }) => [
+        styles.buttonContainer,
+        pressed ? styles.pressedButtonContainer : undefined,
+        props.style,
+      ]}
+    >
+      <Text style={styles.buttonText}>{title}</Text>
+    </Pressable>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  contentContainer: {
+    padding: 16,
+    gap: 32,
+  },
+  controlsContainer: {
+    gap: 8,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: 'black',
+    padding: 8,
+  },
+  controlsTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  controlsInnerContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 8,
+  },
+  buttonContainer: {
+    padding: 8,
+    borderRadius: 4,
+    backgroundColor: 'blue',
+  },
+  pressedButtonContainer: {
+    backgroundColor: 'teal',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
