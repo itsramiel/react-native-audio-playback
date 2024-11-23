2af6780cb5f788d7fb427093e62ecd15dd9af8c8
Rami Elwan <ramielwan48@gmail.com>
2024-11-23 19:44:27 +0100

    fix: fix android old arch

--- android/src/oldarch/AudioPlaybackSpec.kt
+++ android/src/oldarch/AudioPlaybackSpec.kt
@@ -1,11 +1,45 @@
 package com.audioplayback
 
+import com.facebook.proguard.annotations.DoNotStrip
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableArray
+
 
 abstract class AudioPlaybackSpec internal constructor(context: ReactApplicationContext) :
   ReactContextBaseJavaModule(context) {
 
-  abstract fun multiply(a: Double, b: Double, promise: Promise)
+  @ReactMethod
+  @DoNotStrip
+  abstract fun setupAudioStream(sampleRate: Double, channelCount: Double)
+
+  @ReactMethod
+  @DoNotStrip
+  abstract fun openAudioStream()
+
+  @ReactMethod
+  @DoNotStrip
+  abstract fun closeAudioStream()
+
+  @ReactMethod
+  @DoNotStrip
+  abstract fun loopSounds(arg: ReadableArray)
+
+  @ReactMethod
+  @DoNotStrip
+  abstract fun playSounds(arg: ReadableArray)
+
+  @ReactMethod
+  @DoNotStrip
+  abstract fun seekSoundsTo(arg: ReadableArray)
+
+  @ReactMethod
+  @DoNotStrip
+  abstract fun unloadSound(id: String)
+
+  @ReactMethod
+  @DoNotStrip
+  abstract fun loadSound(uri: String, promise: Promise)
 }
