a4e27bc6f43405386820f5de4235321477344b84
Rami Elwan <ramielwan48@gmail.com>
2024-12-05 18:22:21 +0100

    feat: use better sounding sounds

--- example/src/hooks/usePlayers.ts
+++ example/src/hooks/usePlayers.ts
@@ -16,26 +16,26 @@ export function usePlayers(): {
 
   const loadPlayers = useCallback(async () => {
     const player1 = await AudioManager.shared.loadSound(
-      require('../assets/bamboo.mp3')
+      require('../assets/game-track.mp3')
     );
 
     const player2 = await AudioManager.shared.loadSound(
-      require('../assets/swords.mp3')
+      require('../assets/laser-shot.mp3')
     );
 
     const player3 = await AudioManager.shared.loadSound(
-      require('../assets/coins.mp3')
+      require('../assets/music-beat.mp3')
     );
 
     const player4 = await AudioManager.shared.loadSound(
-      require('../assets/axe.mp3')
+      require('../assets/clap.mp3')
     );
 
     const playerObjects = [
-      { player: player1, title: 'Bamboo', selected: false },
-      { player: player2, title: 'Swords', selected: false },
-      { player: player3, title: 'Coins', selected: false },
-      { player: player4, title: 'Axe', selected: false },
+      { player: player1, title: 'Game Track', selected: false },
+      { player: player2, title: 'Laser Shot', selected: false },
+      { player: player3, title: 'Music Beat', selected: false },
+      { player: player4, title: 'Clap', selected: false },
     ].filter((p) => p.player) as Array<TPlayerObject>;
 
     setPlayerObjects(playerObjects);
