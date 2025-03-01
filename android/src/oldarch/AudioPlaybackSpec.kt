3337cfc6a9c31f3e86bd51deca4de616f395d4a9
Rami Elwan <ramielwan48@gmail.com>
2025-03-01 09:44:26 +0100

    fix: fix android old arch (#33)

--- android/src/oldarch/AudioPlaybackSpec.kt
+++ android/src/oldarch/AudioPlaybackSpec.kt
@@ -4,13 +4,14 @@ import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.bridge.WritableMap
 
 
 abstract class AudioPlaybackSpec internal constructor(context: ReactApplicationContext) :
   ReactContextBaseJavaModule(context) {
 
-  abstract fun setupAudioStream(sampleRate: Double, channelCount: Double): WritableMap
+  abstract fun setupAudioStream(options: ReadableMap): WritableMap
 
   abstract fun openAudioStream(): WritableMap
 
@@ -29,4 +30,6 @@ abstract class AudioPlaybackSpec internal constructor(context: ReactApplicationC
   abstract fun unloadSound(id: String)
 
   abstract fun loadSound(uri: String, promise: Promise)
+
+  abstract fun getStreamState(): Double
 }
